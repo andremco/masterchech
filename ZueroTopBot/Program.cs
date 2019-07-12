3eaf866638d51b7b25d8ee016407c9663dde0100
andre.militao <andre.militao@cassi.com.br>
2019-07-12 14:38:28 -0300

    Log for app

--- ZueroTopBot/Program.cs
+++ ZueroTopBot/Program.cs
@@ -13,26 +13,39 @@ namespace ZueroTopBot
     public class Program
     {
         static IConfigurationRoot Configuration;
+        static ILogger<Program> Logger;
 
         public static void Main(string[] args)
         {
+            ConfigurationBuilder();
 
+            Log();
 
-            var builder = new ConfigurationBuilder()                            .SetBasePath(Directory.GetCurrentDirectory())                            .AddJsonFile("appsettings.json");
-            Configuration = builder.Build();
-
-            Console.WriteLine($"Bot key: {Configuration["BotKey"]}");
+            Logger.LogInformation($"Bot key: {Configuration["BotKey"]}");
 
             BuildWebHost(args).Run();
+        }
+
+        public static void ConfigurationBuilder()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
+        public static void Log()
+        {
+            var logFactory = new LoggerFactory()
+                .AddConsole(LogLevel.Information)
+                .AddDebug();
+
+            Logger = logFactory.CreateLogger<Program>();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .ConfigureLogging(logging =>
-                {
-                    logging.AddConsole();
-                })
                 .Build();
     }
 }
--- ZueroTopBot/Startup.cs
+++ ZueroTopBot/Startup.cs
@@ -15,33 +15,29 @@ namespace ZueroTopBot
     //https://medium.com/@fernando.abreu/utilizando-log-em-asp-net-core-171e90732ec5
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(builder => builder
+                .AddConsole()
+                .AddDebug());
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                logger.LogInformation("This is awesome!");
-                await context.Response.WriteAsync("Hello World!");
-            });
 
             app.UseMvc();
         }
