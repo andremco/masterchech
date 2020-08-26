77310166f34140711d2926e6c0290139ac4fc310
andremco1992 <andremco1992@outlook.com>
2020-08-25 21:46:56 -0300

    estrutura

---
+++ src/api/MasterChechBotWebApi/BotControl/BotControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Core.Context;
+using Core.Enum;
+using Core.Messages;
+using Core.Repositories;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace MasterChechBotWebApi.BotControl
+{
+    public static class BotControl
+    {
+    }
+}
