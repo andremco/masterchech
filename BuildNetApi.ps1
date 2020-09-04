ccc5a4339bf2ebf7cb324e107c2d1dc35e9057c2
andremco1992 <andremco1992@outlook.com>
2020-09-04 20:57:20 -0300

    code coverage coverlet

--- BuildNetApi.ps1
+++ BuildNetApi.ps1
@@ -29,7 +29,8 @@ Function UnitTestsNet{
 
 	try {
 		exec {
-			& dotnet test -nologo -v $verbosity --logger:trx --results-directory $test_dir --no-build --no-restore --configuration $projectConfig
+			& dotnet test -nologo -v $verbosity --logger:trx --results-directory $test_dir --no-build --no-restore --configuration $projectConfig /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=$test_dir\Coverage\
+			& dotnet reportgenerator "-reports:$test_dir\Coverage\coverage.cobertura.xml" "-targetDir:$test_dir\Coverage\Reports" -tag:$version -reportTypes:htmlInline
 		}
 	}
 	finally {
@@ -42,7 +43,8 @@ Function IntegrationTestNet{
 
 	try {
 		exec {
-			& dotnet test -nologo -v $verbosity --logger:trx --results-directory $test_dir --no-build --no-restore --configuration $projectConfig
+			& dotnet test -nologo -v $verbosity --logger:trx --results-directory $test_dir --no-build --no-restore --configuration $projectConfig /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=$test_dir\Coverage\
+			& dotnet reportgenerator "-reports:$test_dir\Coverage\coverage.cobertura.xml" "-targetDir:$test_dir\Coverage\Reports" -tag:$version -reportTypes:htmlInline
 		}
 	}
 	finally {
