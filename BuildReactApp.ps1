1d957144450ec42168bc7db36c2bbd45b4c81029
andremco1992 <andremco1992@outlook.com>
2020-09-15 17:03:23 -0300

    --no-optional

--- BuildReactApp.ps1
+++ BuildReactApp.ps1
@@ -5,7 +5,7 @@ Function PrivateBuildReact{
 
 	exec {
 		& cd $source_web_app_dir
-		& npm install 
+		& npm install --no-optional
 		& npm run build
 		& cd $base_dir
 	}
--- DeployAzureContainer.ps1
+++ DeployAzureContainer.ps1
@@ -1,7 +1,14 @@
-az container create --name webapi --resource-group masterchech `
+$userNameAZCR = (az keyvault secret show --vault-name key-vault-masterchech --name USER-AZCR --query value)
+$passAZCR = (az keyvault secret show --vault-name key-vault-masterchech --name PASS-AZCR --query value)
+$apiKeyHeader = (az keyvault secret show --vault-name key-vault-masterchech --name API-KEY-HEADER --query value)
+$botKeyTelegram = (az keyvault secret show --vault-name key-vault-masterchech --name BOT-KEY-TELEGRAM --query value)
+$connectionString = (az keyvault secret show --vault-name key-vault-masterchech --name CONNECTION-STRING --query value)
+
+az container create --name master-chech-webapi --resource-group masterchech `
                     --image masterchech.azurecr.io/master-chech-webapi:latest `
                     --registry-login-server masterchech.azurecr.io `
-                    --registry-username masterchech `
-                    --registry-password Ua7PUM2hqH7wE/qbu9TGmrmMT9sHzsRt `
-                    --dns-name-label webapi232323 `
-                    --query ipAddress.fqdn
+                    --registry-username $userNameAZCR `
+                    --registry-password $passAZCR `
+                    --dns-name-label master-chech-webapi `
+                    --query ipAddress.fqdn `
+                    --environment-variables ApiKeyValid=$apiKeyHeader BotKey=$botKeyTelegram ConnectionString=$connectionString
