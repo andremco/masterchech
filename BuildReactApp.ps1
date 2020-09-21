60e557e5603359784ab1389b0f8a78ff0da51e3c
andremco1992 <andremco1992@outlook.com>
2020-09-20 21:14:53 -0300

    CreateFileEnvIfNotExitsForReact

--- BuildReactApp.ps1
+++ BuildReactApp.ps1
@@ -16,8 +16,13 @@ Function CIBuildReact{
 
 Function CreateFileEnvIfNotExitsForReact{
 	if (-Not(Test-Path ".env")) {
+		Write-Host "Create .env file"
+		Write-Host "REACT_APP_API_URL=$urlApi`nREACT_APP_API_KEY_HEADER=$apiKeyHeader"
 		exec{
 			Set-Content -Path .env -Value "REACT_APP_API_URL=$urlApi`nREACT_APP_API_KEY_HEADER=$apiKeyHeader"
 		}
+	}
+	else {
+		Write-Host "File .env exists"
 	}
 }
